b8daa411263a207edd61c547b563547f0ce5d431
gillybops <gillybops@optonline.net>
2017-10-22 16:14:10 +0000

    Pagination Added

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,9 @@
 class PlacesController < ApplicationController
 
   def index
-    
-    @places = Place.all
+# Perform a paginated query; use an explicit "per page" limite:
+    @places = Place.paginate(:page => params[:page], :per_page => 3)
 
   end
-
 
 end
