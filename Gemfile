c28da4510c6d95d4908542384953689e10e30d3f
gillybops <gillybops@optonline.net>
2017-10-23 21:37:38 +0000

    Built New Places Form

--- Gemfile
+++ Gemfile
@@ -61,3 +61,6 @@ end
 
 # gem 'will_paginate', '>= 3.1'
 gem 'will_paginate-bootstrap'
+
+# gem 'simple_form', v3.1
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,10 @@ class PlacesController < ApplicationController
   def index
 # Perform a paginated query; use an explicit "per page" limite:
     @places = Place.paginate(:page => params[:page], :per_page => 3)
+  end
 
+  def new
+    @place = Place.new 
   end
 
 end
