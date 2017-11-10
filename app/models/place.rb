a365b6a756b8c550fb4cdbc811774f366ed1623f
gillybops <gillybops@optonline.net>
2017-11-10 08:03:08 +0000

    Setting up the Pipeline for Photos

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,4 +8,5 @@ class Place < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
+  validates :caption, presence: true
 end
