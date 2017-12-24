6c0ef670b12d7eaf436da75c7a6bb083acc96169
gillybops <gillybops@optonline.net>
2017-12-24 21:44:41 +0000

    Removed Caption Validation from Place Model

--- app/models/place.rb
+++ app/models/place.rb
@@ -9,5 +9,4 @@ class Place < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
-  validates :caption, presence: true
 end
